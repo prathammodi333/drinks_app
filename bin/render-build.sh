1e047ccf592e1f137cf77cf4bce94a32e9fce464
prathammodi333 <prathammodi333@gmail.com>
2025-02-28 01:45:16 -0500

    Fixed execution permissions for bin/rails and all bin scripts

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
-
+curl -fsSL https://deb.nodesource.com/setup_16.x | bash -  # Installs Node.js
+apt-get install -y nodejs
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
