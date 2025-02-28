b86c4313792911f92b51fee7d8ebc5b41a2ee7d6
prathammodi333 <prathammodi333@gmail.com>
2025-02-28 01:55:22 -0500

    Set execute permissions for bin scripts

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,6 @@ set -o errexit
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -  # Installs Node.js
 apt-get install -y nodejs
 bundle install
+chmod +x bin/*
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
